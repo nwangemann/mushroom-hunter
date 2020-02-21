5b3c9ada7a0783941bd0edfe40dae9c07bfe3ca5
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-21 15:05:27 -0700

    adds basic layout for component files

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './Main.css'
+
+class Main extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div id="headerParent" >
+        <h1>Main!</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/Components/Post/Post.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './Post.css'
+
+class Post extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div id="headerParent" >
+        <h1>Post!</h1>
+      </div>
+    );
+  }
+}
+
+export default Post;
