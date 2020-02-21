8f4d4d164154c4225e6afb7a4a4600542d3cd7d7
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-21 14:58:48 -0700

    adds header component and basic styling to work with header, adds reset css file

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './Header.css'
+
+class Header extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div id="headerParent" >
+        <h1>Header!</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
