4b439c975be0bdf9d10e93d46278b2133e83e074
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-21 15:03:44 -0700

    adds basic outline for routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import './reset.css'
 import "./App.css";
 import Header from './Components/Header/Header'
+import { Switch, Route} from 'react-router-dom'
+
 
 class App extends React.Component {
   render() {
     return <div className="App">
       <Header /> 
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/main" component={Main} />
+        <Route path="/post" component={Post} />
+        <Route path="/detail" component={Detail} />
+      </Switch>
     </div>;
   }
 }
