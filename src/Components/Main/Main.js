2ad66c7c10830283d4e4326d90ac7dcfa745e864
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-02-27 17:18:21 -0700

    adds styling and mobile responsiveness

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import "./Main.scss";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-import { removeLoginView, getSession, getDetailViewID } from "../../redux/reducer";
+import {
+  removeLoginView,
+  getSession,
+  getDetailViewID
+} from "../../redux/reducer";
 import axios from "axios";
 
 class Main extends Component {
@@ -39,30 +43,39 @@ class Main extends Component {
     this.getUserPosts(this.props.user.user_id);
   };
 
-  setDetail = (e) => {
-    let id = e.target.value
-    console.log('setdetail ID', id)
-    this.props.getDetailViewID(id)
-    this.props.history.push('/detail')
-  }
+  setDetail = e => {
+    let id = e.target.value;
+    console.log("setdetail ID", id);
+    this.props.getDetailViewID(id);
+    this.props.history.push("/detail");
+  };
 
   render() {
     const mappedPosts = this.state.posts.map(post => {
       return (
         <div key={post.post_id} className="postContainer">
           <div className="dashboardText">
-            <h2>Species: {post.species}</h2>
-            <h2>Edible: {post.edible}</h2>
-            <h2>Date: {post.date}</h2>
-            <p>Description: {post.description}</p>
-            <button
-              className="mainViewButton"
-              onClick={this.deletePost}
-              value={post.post_id}
-            >
-              Delete
-            </button>
-              <button className="mainViewButton" value={post.post_id} onClick={this.setDetail} >Post Details</button>
+            <div>
+              <h2 className="textBoxElem" >Species: {post.species}</h2>
+              <h2 className="textBoxElem" >Edible: {post.edible}</h2>
+              <h2 className="textBoxElem" >Date: {post.date}</h2>
+              <p className="textBoxElem" >Description: {post.description}</p>
+            </div>
+            <div className="textBoxElem" >
+              <button
+                className="mainViewButton"
+                onClick={this.deletePost}
+                value={post.post_id}
+              >
+                Delete
+              </button><button
+                className="mainViewButton"
+                value={post.post_id}
+                onClick={this.setDetail}
+              >
+                Post Details
+              </button>
+            </div>
           </div>
           <div>
             <img className="postPhoto" alt="mushroom" src={post.image_url} />
@@ -70,17 +83,16 @@ class Main extends Component {
         </div>
       );
     });
-    return <div>
-      <div id="spaceTaker">
-
+    return (
+      <div>
+        <div id="spaceTaker"></div>
+        {this.props.user ? (
+          <div className="postParent">{mappedPosts}</div>
+        ) : (
+          <Redirect to="/" />
+        )}
       </div>
-    {this.props.user
-      ?
-    <div className="postParent">{mappedPosts}</div>
-      :
-      <Redirect to='/' /> 
-    }
-    </div>
+    );
   }
 }
 
