81b124b36ef82818e617b7ffb88a6885fd155df6
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-03-04 10:06:29 -0700

    adds basics of mobile responsiveness for guide component, adds search bar and change handler for storing search term on state

--- src/Components/Guide/Guide.js
+++ src/Components/Guide/Guide.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./Guide.scss";
 import axios from "axios";
 import { connect } from "react-redux";
-import { Redirect, Link } from "react-router-dom";
 import { getSession } from "../../redux/reducer";
 
 class Guide extends Component {
@@ -10,7 +9,8 @@ class Guide extends Component {
     super(props);
 
     this.state = {
-      guide: []
+      guide: [],
+      search: ""
     };
   }
 
@@ -26,33 +26,56 @@ class Guide extends Component {
     });
   };
 
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log('state', this.state)
+  };
+
   render() {
     const mappedGuide = this.state.guide.map(entry => {
       return (
         <div className="entryParent">
           <div className="displayedTextParent">
-          <h1>{entry.species}</h1>
-          <p>Latin name: {entry.scientific_name}</p>
-          <p>Edibility: {entry.edible}</p>
-          <p>Eating Notes: {entry.eating_notes}</p>
-          <p>Season: {entry.season}</p>
-          <p>Frequency: {entry.frequency}</p>
-          <p>
-            Description:
-            <div>{entry.description}</div>
-          </p>
+            <h1>{entry.species}</h1>
+            <p>Latin name: {entry.scientific_name}</p>
+            <p>Edibility: {entry.edible}</p>
+            <p>Eating Notes: {entry.eating_notes}</p>
+            <p>Season: {entry.season}</p>
+            <p>Frequency: {entry.frequency}</p>
+            <p>
+              Description:
+              <div>{entry.description}</div>
+            </p>
           </div>
           <div className="photoParent">
-          <img
-            src={entry.image_url}
-            alt="mushroomIDExample"
-            className="guidePhoto"
-          />
+            <img
+              src={entry.image_url}
+              alt="mushroomIDExample"
+              className="guidePhoto"
+            />
           </div>
         </div>
       );
     });
-    return <div className="guideContainer" >{mappedGuide}</div>;
+    return (
+      <div className="guideContainer">
+        <div className="searchBar">
+          <div className="searchBarInner">
+            <label>Search:</label>
+            <input
+              onChange={this.changeHandler}
+              placeholder="Try searching by species name, edibility, etc!"
+              type="text"
+              name="search"
+              value={this.state.search}
+            />
+          </div>
+        </div>
+        {mappedGuide}
+      </div>
+    );
   }
 }
 
